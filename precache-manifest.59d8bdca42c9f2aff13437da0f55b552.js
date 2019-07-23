5d8cfa8e7a875f65118bb225578a959a8785e90e
Fabio Pinheiro <fabio.pinheiro@confitec.com.br>
2019-07-23 19:42:26 -0300

    Updates

---
+++ precache-manifest.59d8bdca42c9f2aff13437da0f55b552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991209943a3e32ee950df24467c28c70",
+    "url": "/e-simples-front/index.html"
+  },
+  {
+    "revision": "8bde811dfe3973741a99",
+    "url": "/e-simples-front/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "0b2b75eb7a7942cec739",
+    "url": "/e-simples-front/static/css/main.9edc659e.chunk.css"
+  },
+  {
+    "revision": "8bde811dfe3973741a99",
+    "url": "/e-simples-front/static/js/2.62d28de2.chunk.js"
+  },
+  {
+    "revision": "0b2b75eb7a7942cec739",
+    "url": "/e-simples-front/static/js/main.be2d2c00.chunk.js"
+  },
+  {
+    "revision": "8c118dc728c67b655c46",
+    "url": "/e-simples-front/static/js/runtime~main.a7548dc3.js"
+  }
+]);
